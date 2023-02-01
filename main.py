2c354084876fcb3894fa81ebbc22f871a752b3a2
christuss <szendefi.krisztofer@gmail.com>
2023-02-01 12:30:30 +0100

    osszegzes

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 #megoldas
 def eredmeny(jatekosPont, gepPont):
-    if jatekosPont > 21:
-        print("Vesztettél!")
-    elif gepPont > 21:
-        print("Nyertél!")
 
+    if osszegzes(jatekosPont) > 21:
+        print("A játékos vesztett!")
+    elif osszegzes(gepPont) > 21:
+        print("A gép vesztett!")
+
+def osszegzes(lista):
+    osszeg = 0
+    for i in range(len(lista)):
+        osszeg += lista[i]
+    return osszeg
 #teszt
