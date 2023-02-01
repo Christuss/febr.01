7d7bee6c648c2ad35e826676703c876008fd4ee8
christuss <szendefi.krisztofer@gmail.com>
2023-02-01 12:52:22 +0100

    eredmeny visszateresi ertek adas

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 
-#megoldas
-def eredmeny(jatekosPont, gepPont):
-
-    if osszegzes(jatekosPont) > 21:
-        print("A játékos vesztett!")
-    elif osszegzes(gepPont) > 21:
-        print("A gép vesztett!")
+# megoldas
+def eredmeny(jatekosLapok: [int], gepLapok: [int]):
+    if osszegzes(jatekosLapok) > 21:
+        return "A játékos vesztett!"
+    elif osszegzes(gepLapok) > 21:
+        return "A gép vesztett!"
 
 def osszegzes(lista):
     osszeg = 0
     for i in range(len(lista)):
         osszeg += lista[i]
     return osszeg
-#teszt
+# teszt
