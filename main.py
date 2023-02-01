5c36f163aaee64ca5426036398bce6cd0ad02d11
christuss <szendefi.krisztofer@gmail.com>
2023-02-01 12:16:06 +0100

    eredmeny metodus, valtozo atadas, printalas

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
 #megoldas
-def eredmeny():
-    pass
+def eredmeny(jatekosPont, gepPont):
+    if jatekosPont > 21:
+        print("Vesztettél!")
+    elif gepPont > 21:
+        print("Nyertél!")
+
 #teszt
