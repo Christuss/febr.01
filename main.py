b2e28c046f2113c945af0f76e5cb91ceb7ee7b36
christuss <szendefi.krisztofer@gmail.com>
2023-02-02 14:06:25 +0100

    eredmeny minta szerinti elkeszitese

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 
 # megoldas
-def eredmeny(jatekosLapok: [int], gepLapok: [int]):
-    if osszegzes(jatekosLapok) > 21:
-        return "A játékos vesztett!"
-    elif osszegzes(gepLapok) > 21:
-        return "A gép vesztett!"
+def eredmeny(jatekosLista, gepLista):
+    jatekosPont = osszegzes(jatekosLista)
+    gepPont = osszegzes(gepLista)
+    jatekosDarab = len(jatekosLista)
+    gepDarab = len(gepLista)
+    if jatekosPont <= 21 and gepPont <= 21:
+        if jatekosPont > gepPont:
+            s = "A játékos nyert"
+        elif gepPont > jatekosPont:
+            s = "A gép nyert"
+        elif gepPont == jatekosPont:
+            if jatekosDarab < gepDarab:
+                s = "A játékos nyert"
+            elif jatekosDarab > gepDarab:
+                s = "A gép nyert"
+            else:
+                s = "Döntetlen, osztoztok a nyereségen"
+    elif jatekosPont > 21:
+        s = "A játékos vesztett"
+    if gepPont > 21:
+        s = "A gép vesztett"
+    if jatekosPont > 21 and gepPont > 21:
+        s = "döntetlen, a Ház nyert!"
+    return s
 
 def osszegzes(lista):
     osszeg = 0
@@ -16,7 +35,7 @@ def jatekos_vesztett_teszt():
     jatekos = [5, 5, 5, 5, 5]
     gep = [8, 6]
     kapott = eredmeny(jatekos, gep)
-    vart = "A játékos vesztett!"
+    vart = "A játékos vesztett"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
@@ -26,7 +45,7 @@ def gep_vesztett_teszt():
     jatekos = [8, 6]
     gep = [5, 5, 5, 5, 5]
     kapott = eredmeny(jatekos, gep)
-    vart = "A gép vesztett!"
+    vart = "A gép vesztett"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
@@ -37,11 +56,7 @@ def jatekos_vesztett_teszt_2():
     jatekos = [8, 6]
     gep = [5, 5, 5, 5]
     kapott = eredmeny(jatekos, gep)
-    if osszegzes(jatekos) > osszegzes(gep):
-        kapott = "A játékos nyert!"
-    elif osszegzes(gep) > osszegzes(jatekos):
-        kapott = "A játékos vesztett!"
-    vart = "A játékos vesztett!"
+    vart = "A gép nyert"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
@@ -52,11 +67,7 @@ def gep_vesztett_teszt_2():
     jatekos = [8, 8]
     gep = [5, 5, 9]
     kapott = eredmeny(jatekos, gep)
-    if osszegzes(jatekos) > osszegzes(gep):
-        kapott = "A játékos nyert!"
-    elif osszegzes(gep) > osszegzes(jatekos):
-        kapott = "A gép nyert!"
-    vart = "A gép nyert!"
+    vart = "A gép nyert"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
@@ -67,12 +78,7 @@ def gep_vesztett_teszt_3():
     jatekos = [8, 8]
     gep = [5, 5, 6]
     kapott = eredmeny(jatekos, gep)
-    if osszegzes(jatekos) == osszegzes(gep):
-        if len(jatekos) > len(gep):
-            kapott = "A gép nyert!"
-        elif len(gep) > len(jatekos):
-            kapott = "A játékos nyert!"
-    vart = "A játékos nyert!"
+    vart = "A játékos nyert"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
@@ -83,12 +89,7 @@ def jatekos_vesztett_teszt_3():
     jatekos = [4, 6, 6]
     gep = [8, 8]
     kapott = eredmeny(jatekos, gep)
-    if osszegzes(jatekos) == osszegzes(gep):
-        if len(jatekos) > len(gep):
-            kapott = "A gép nyert!"
-        elif len(gep) > len(jatekos):
-            kapott = "A játékos nyert!"
-    vart = "A gép nyert!"
+    vart = "A gép nyert"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
@@ -99,14 +100,7 @@ def dontetlen_vesztettek_teszt():
     jatekos = [11, 11]
     gep = [11, 11]
     kapott = eredmeny(jatekos, gep)
-    if osszegzes(jatekos) == osszegzes(gep):
-        if len(jatekos) > len(gep):
-            kapott = "A gép nyert!"
-        elif len(gep) > len(jatekos):
-            kapott = "A játékos nyert!"
-        elif osszegzes(jatekos) > 21:
-            kapott = "Döntetlen, vesztettetek!"
-    vart = "Döntetlen, vesztettetek!"
+    vart = "döntetlen, a Ház nyert!"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
@@ -117,21 +111,11 @@ def dontetlen_nyertek_teszt():
     jatekos = [11, 9]
     gep = [11, 9]
     kapott = eredmeny(jatekos, gep)
-    if osszegzes(jatekos) == osszegzes(gep):
-        if len(jatekos) > len(gep):
-            kapott = "A gép nyert!"
-        elif len(gep) > len(jatekos):
-            kapott = "A játékos nyert!"
-        elif osszegzes(jatekos) > 21:
-            kapott = "Döntetlen, vesztettetek!"
-        else:
-            kapott = "Döntetlen, nyertetek!"
-    vart = "Döntetlen, nyertetek!"
+    vart = "Döntetlen, osztoztok a nyereségen"
     if kapott == vart:
         print("A teszt sikeres!")
     else:
         print("A teszt megbukott!")
-
 
 
 def tesztek():
